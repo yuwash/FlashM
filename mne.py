747b1dd683e27bc64749704a79367e6b1f9b1845
Yushin Washio <wash10@safe-mail.net>
2011-09-27 20:19:52 +0200

    mnemosyne-compatible quiz file I/O module mne.py for FlashM 0.1.0

---
+++ mne.py
@@ -0,0 +1,53 @@
+# mne.py quiz file handling class for FlashM 0.1.0
+# quiz files of Mnemosyne (mnemosyne.org)
+#
+# This only works within flashm.py
+# see README file for copyright information and version history
+
+import flashmquiz, zipfile, os, xml.dom.minidom
+
+def load(filename, name):
+    zf = zipfile.ZipFile(filename)
+    xmlfilename = zf.namelist()[0]
+    sidetext = (lambda node:
+        unicode(node.firstChild.data.strip().encode('utf_8'), 'utf_8')
+    )
+    cards = xml.dom.minidom.parse(zf.open(xmlfilename)).getElementsByTagName('item')
+    zf.close()
+    set = []
+    for card in cards:
+        set.append([
+            sidetext(card.getElementsByTagName('Q').item(0)),
+            sidetext(card.getElementsByTagName('A').item(0))
+        ])
+    return flashmquiz.quiz(name, set)
+    
+def dump(quiz, filename):
+    #@param quiz has the type of flashmquiz.quiz
+    mxml = xml.dom.minidom.getDOMImplementation().createDocument(None, 'mnemosyne', None)
+    mxml.documentElement.appendChild(
+        mxml.createComment('This is a quiz file for Mnemosyne, created by FlashM')
+    )
+    catnode = mxml.createElement('category')
+    namenode = mxml.createElement('name')
+    namenode.appendChild(mxml.createTextNode(quiz.name))
+    catnode.appendChild(namenode)
+    mxml.documentElement.appendChild(catnode)
+    for card in quiz.set:
+        itemnode = mxml.createElement('item')
+        for i in (0,1):
+            if i: #== 1
+                sidenode = mxml.createElement('A')
+            else: #i == 0
+                sidenode = mxml.createElement('Q')
+            sidenode.appendChild(mxml.createTextNode(card[i].encode('utf_8')))
+            itemnode.appendChild(sidenode)
+        mxml.documentElement.appendChild(itemnode)
+    mfname = quiz.name +'.XML'
+    mf = open(mfname, 'w')
+    mxml.writexml(mf, addindent = '  ', newl = os.linesep)
+    mf.close()
+    zf = zipfile.ZipFile(filename, 'w')
+    zf.write(mfname)
+    zf.close()
+    os.remove(mfname)
